c4478b2f162026fa602ffd2e4ebb4493bfcf8ccb
MehmetAkifff <98951398+MehmetAkifff@users.noreply.github.com>
2022-10-08 15:49:01 +0300

    Update findingNumber.java

--- findingNumber.java
+++ findingNumber.java
@@ -1,4 +1,5 @@
 public class findingNumber {
+    //That program is useful to find a number in an array
 
     public static void main(String[] args) {
         int[] numbers = new int[]{1, 2, 5, 7, 9, 0};
